4e86fee22b0017343c16a633796ed3c4b8a0e9f0
SezerMizrak <sezermizrak@icloud.com>
2024-01-30 22:04:51 +0300

    First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Title from './Title';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Title.js
@@ -0,0 +1,11 @@
+
+
+export default function Title () {
+  return (
+    <div>
+    <h1 className = "title">Ja<span>mmm</span>ing</h1>
+  </div>
+
+  );
+ 
+}
